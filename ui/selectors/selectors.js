a1bf612b1d72f235e89195e27a2257b94c71adf3
David Walsh <davidwalsh83@gmail.com>
2024-03-04 11:03:50 -0600

    Debug

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -1108,6 +1108,7 @@ export function getShowWhatsNewPopup(state) {
 }
 
 export function getCurrentPopupId(state) {
+  console.log("App state is: ", state.appState);
   return state.appState.currentPopupId;
 }
 
